f1ea10f28e2ce2b37096eed74332a61646c84193
AndreaAldana <andrealdanapaz@gmail.com>
2020-11-18 20:51:16 -0300

    Backend terminado, crud completo

--- src/database.js
+++ src/database.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
-
+const express = require('express');
+const app = express();
+var port = process.env.PORT || 3900;
 const MongoClient = require('mongodb').MongoClient;
 
 
 
-mongoose.connect('mongodb+srv://AndreaAldana:Paralelepipedo_98@cluster0.ipklg.mongodb.net/MERNTasks? retryWrites=true&w=majority', { useNewUrlParser: true })
+mongoose.connect('mongodb+srv://AndreaAldana:Paralelepipedo_98@cluster0.ipklg.mongodb.net/MERNTasks? retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
             console.log('La conexion a mongodb se realizo correctamente!');
 
